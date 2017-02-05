097b5367d886c7cb4c6112997415ea63be39af3a
Pankaj <pankajisadeveloper@gmail.com>
2017-02-05 15:20:30 +0530

    Update main_file.py
    Comlpeted raw-integrated code

--- Python/GPIO Control/main_file.py
+++ Python/GPIO Control/main_file.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as gpio
+import os
+import picamera
 from time import sleep
 
 #Defining GPIO pin numbers 
@@ -30,6 +32,20 @@ def switch_off():
     gpio.output(pin1, 0)
     gpio.output(pin2, 0)
     sleep(2)
+    
+def click_picture():
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 15
+    camera.capture('image.jpg')
+    camera.start_preview()
+    camera.vflip = False
+    camera.hflip = False
+    camera.brightness = 50
+    camera.start_preview()
+    camera.capture('image.jpg')
+    camera.stop_preview()
+    print ("Clicked Image")
 
 def move_to_cell(index):
     global pin1
@@ -96,10 +112,49 @@ try:
     reset()
     # Infinity Loop
     while (1):
-        switch_on()
-        print "ON"
-        switch_off()
-        print "OFF"
+        os.popen("nc -l 2999 > received.txt")
+        print ("Received data")
+        f = open("received.txt")
+        next = (f.readline()).strip()
+        if (next == '01'):
+            move_to_cell(1)
+        elif (next == '02'):
+            move_to_cell(2)
+        elif (next == '03'):
+            move_to_cell(3)
+        elif (next == '04'):
+            move_to_cell(4)
+        elif (next == '05'):
+            move_to_cell(5)
+        elif (next == '06'):
+            move_to_cell(6)
+        elif (next == '07'):
+            move_to_cell(7)
+        elif (next == '08'):
+            move_to_cell(8)
+        elif (next == '09'):
+            move_to_cell(9)
+        elif (next == '10'):
+            reset()
+        elif (next == '11'):
+            camera_servo_movement(0)
+        elif (next == '12'):
+            camera_servo_movement(90)
+        elif (next == '13'):
+            camera_servo_movement(180)
+        elif (next == '00'):
+            click_picture()
+        os.popen("cat image.jpg | nc 192.168.1.108 | 2999")
+        print ("Image sent")
+        with open("send.txt", "a") as f
+            f.write("1\n")
+        os.popen("cat send.txt | nc 192.168.1.108 | 2999")
+        print ("number sent")
+#         switch_on()
+#         print "ON"
+#         switch_off()
+#         print "OFF"
+
 except KeyboardInterrupt:
     gpio.cleanup()
 finally:
