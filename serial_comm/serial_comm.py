701c19ca93b958f75ddd6fae79c254350b1b9730
raghav1875 <raghav97singh.rs@gmail.com>
2017-08-13 00:52:37 +0530

    Added arduino code and python serial control script

--- serial_comm/serial_comm.py
+++ serial_comm/serial_comm.py
@@ -1,12 +1,11 @@
 import serial, time
-arduino = serial.Serial('/dev/ttyACM1', 9600)
-
+arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
-	option = int(raw_input("Enter 1"))
+	option = raw_input("Enter a-i or l,r or z.")
 	write_serial = arduino.write(str(option))
 	arduino.read()
-	read_serial = arduino.read()	
+	read_serial = arduino.read()
 	print read_serial
 
 
