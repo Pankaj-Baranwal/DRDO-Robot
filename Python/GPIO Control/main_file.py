bbf0e755f0458e40e233a2fa2bea93a641672905
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-08-16 21:23:49 +0530

    Updated RPi to Arduino

--- Python/GPIO Control/main_file.py
+++ Python/GPIO Control/main_file.py
@@ -1,8 +1,6 @@
-import RPi.GPIO as gpio
 import os
 import picamera
 from time import sleep, time
-
 
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
@@ -14,20 +12,7 @@ counter = 0
 camera.start_preview()
 sleep(0.5)
 
-#Defining GPIO pin numbers 
-pin1 = 6 # This is GPIO6, hence, 5th pin in inner row from ports
-pin2 = 7 # 8th pin in outer row from ports
-pin3 = 8 # 9th pin in outer row from ports
-pin4 = 9 # 10th pin in inner row from ports
 prev_time = 0
-
-# Setting up basic GPIO settings
-gpio.setmode(gpio.BCM)
-gpio.setwarnings(False)
-gpio.setup(pin1, gpio.OUT)
-gpio.setup(pin2, gpio.OUT)
-gpio.setup(pin3, gpio.OUT)
-gpio.setup(pin4, gpio.OUT)
     
 def click_picture():
     global camera
@@ -42,81 +27,59 @@ def click_picture():
     print ("Image sent")
 
 def move_to_cell(index):
-    print ('GOT IN')
     global pin1
     global pin2
     global pin3
     global pin4
-    wait_time = 4
     if (index == 1):
-        gpio.output(pin1, 1)
+        option = 'a'
     elif (index == 2):
-        gpio.output(pin2, 1)
+        option = 'b'
     elif (index == 3):
-        gpio.output(pin1, 1)
-        gpio.output(pin2, 1)
+        option = 'c'
     elif (index == 4):
-        gpio.output(pin3, 1)
+        option = 'd'
     elif (index == 5):
-        gpio.output(pin1, 1)
-        gpio.output(pin3, 1)
+        option = 'e'
     elif (index == 6):
-        gpio.output(pin2, 1)
-        gpio.output(pin3, 1)
+        option = 'f'
     elif (index == 7):
-        gpio.output(pin1, 1)
-        gpio.output(pin2, 1)
-        gpio.output(pin3, 1)
+        option = 'g'
     elif (index == 8):
-        gpio.output(pin4, 1)
+        option = 'h'
     elif (index == 9):
-        gpio.output(pin1, 1)
-        gpio.output(pin4, 1)
-    sleep(wait_time)
-    camera_servo_movement(0)
-    camera_servo_movement(90)
-    camera_servo_movement(180)
+        option = 'i'
+    write_serial = arduino.write(str(option))
+
+    arduino.read()
+    read_serial = arduino.read()
+    if (read_serial == 't'):    
+        camera_servo_movement(0)
+        camera_servo_movement(90)
+        camera_servo_movement(180)
+    else:
+        print ("Something went wrong while processing instruction!")
         
 def camera_servo_movement(position):
-    global pin1
-    global pin2
-    global pin3
-    global pin4
-    wait_time = 1.5
     if (position == 0):
-        gpio.output(pin2, 1)
-        gpio.output(pin4, 1)
-	wait_time = 2.5
+        option = 'r'
     elif (position == 90):
-        gpio.output(pin1, 1)
-        gpio.output(pin2, 1)
-        gpio.output(pin4, 1)
+        option = 'z'        
     elif (position == 180):
-        gpio.output(pin3, 1)
-        gpio.output(pin4, 1)
-    print (position)
-    sleep(wait_time)
-    click_picture()
-        
-def reset():
-    global pin1
-    global pin2
-    global pin3
-    global pin4
-    gpio.output(pin1, 0)
-    gpio.output(pin2, 0)
-    gpio.output(pin3, 0)
-    gpio.output(pin4, 0)
-    sleep(1)
+        option = 'l'        
+    write_serial = arduino.write(str(option))
+
+    arduino.read()
+    read_serial = arduino.read()
+    if (read_serial == 't'):    
+        click_picture()
+    else:
+        print ("Something went wrong while processing instruction!")
     
 current_milli_time = lambda: int(round(time() * 1000))
     
-
 # Putting under try block as need to clean up pins on keyboard interrupt
 try:
-    # Turned off by default
-    reset()
-    # Infinity Loop
     camera_servo_movement(0)
     camera_servo_movement(90)
     camera_servo_movement(180)
@@ -151,7 +114,7 @@ try:
         elif (next == '9'):
             move_to_cell(9)
         elif (next == '10'):
-            reset()
+            # reset
         elif (next == '11'):
             camera_servo_movement(0)
         elif (next == '12'):
@@ -159,14 +122,8 @@ try:
         elif (next == '13'):
             camera_servo_movement(180)
         elif (next == '0'):
-            camera_servo_movement(0)
-            camera_servo_movement(90)
-            camera_servo_movement(180)
-#         switch_on()
-#         print "ON"
-#         switch_off()
-#         print "OFF"
-
+            #reset
+            
 except KeyboardInterrupt:
     gpio.cleanup()
     camera.stop_preview()
